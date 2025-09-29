9fb7fb730982102d27907bb4fa14175cdf315e22
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-09-29 16:38:15 +0530

    Improve Work tech stack mobile grid responsiveness

--- reactjs/src/components/Work.jsx
+++ reactjs/src/components/Work.jsx
@@ -330,7 +330,7 @@ export default function Work() {
                         key={activeTab}
                         className={`grid my-10 gap-5 ${
                             activeTab === "techStack"
-                                ? "grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"
+                                ? "grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6"
                                 : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
                         }`}
                         initial="hidden"
